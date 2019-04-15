96614bda56a992adca858f224b69813ba2c0c62d
Alex Vassiliou <alex@20diamond.com>
2019-04-16 00:42:39 +1000

    fix model to update existing records from CSV

--- app/models/good.rb
+++ app/models/good.rb
@@ -1,16 +1,22 @@
 class Good < ApplicationRecord
   require 'csv'
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true
   validates :category, presence: true
   validates :source, presence: true
   validates :destination, presence: true
-  validates :consignment, presence: true
+  validates :consignment, presence: true, uniqueness: true
   validates :entry_at, presence: true
 
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
-      Good.create! row.to_hash
+      goods_hash = row.to_hash
+      existing_records = where(consignment: goods_hash["consignment"])
+
+      if existing_records.present?
+        existing_records.first.update_attributes(goods_hash)
+      else
+        create!(goods_hash)
+      end
     end
   end
-
 end
