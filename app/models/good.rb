c5d789b8a373458056112977e8b7b182f6f7dba1
Alex Vassiliou <alex@20diamond.com>
2019-04-15 22:39:57 +1000

    model validations added

--- app/models/good.rb
+++ app/models/good.rb
@@ -1,2 +1,10 @@
 class Good < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  validates :category, presence: true
+  validates :source, presence: true
+  validates :destination, presence: true
+  validates :consignment, presence: true
+  validates :entry_at, presence: true
+
+
 end
